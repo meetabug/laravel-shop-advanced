5f3cfbef25f0568305356e91658871707f3effc1
meetabug <1272526724@qq.com>
2020-07-24 16:47:47 +0800

    同步商品数据至 Elasticsearch

--- app/Admin/Controllers/CommonProductsController.php
+++ app/Admin/Controllers/CommonProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Jobs\SyncOneProductToES;
 use App\Models\Product;
 use Encore\Admin\Controllers\AdminController;
 use App\Models\Category;
@@ -73,6 +74,11 @@ abstract class CommonProductsController extends AdminController
             $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
         });
 
+        $form->saved(function (Form $form) {
+            $product = $form->model();
+            dispatch(new SyncOneProductToES($product));
+        });
+        
         return $form;
     }
 
